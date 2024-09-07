edbfce4920a4972e208eed2046e208487f7da1e3
Ruy1024 <matheus.ruy04@gmail.com>
2024-09-07 13:55:21 -0300

    ruy

--- src/main/java/xadrez/pecas/Peao.java
+++ src/main/java/xadrez/pecas/Peao.java
@@ -20,8 +20,15 @@ public class Peao extends Peca {
 
     @Override
     public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'movimentoValido'");
+        if (cor.equalsIgnoreCase("white")){
+            if (linhaO == 2){
+                if ((linhaD == linhaO + 1 || linhaD == linhaO + 2) && colunaD == colunaO)
+                    return true;
+            }else if (linhaD == linhaO + 1 && colunaD == colunaO) return true;
+        }else if (cor.equalsIgnoreCase("black")){
+            
+        }
+        return false;
     }
 
     @Override
